8ffd686bffdd16ebb8b882ca9246df7acd98ec28
ADBond <48208438+ADBond@users.noreply.github.com>
2025-04-22 12:49:33 +0100

    correct JW expected numerics

--- src/test/scala/uk/gov/moj/dash/linkage/JaroWinklerDistanceTest.scala
+++ src/test/scala/uk/gov/moj/dash/linkage/JaroWinklerDistanceTest.scala
@@ -22,10 +22,10 @@ class JaroWinklerSimilarityTest extends AnyFlatSpec with Matchers {
         distance.call("hippo", "elephant") should equal (0.44)
         distance.call("hippo", "zzzzzzzz") should equal (0.0)
         distance.call("hello", "hallo") should equal (0.88)
-        distance.call("ABC Corporation", "ABC Corp") should equal (0.93)
+        distance.call("ABC Corporation", "ABC Corp") should equal (0.907)
         distance.call("D N H Enterprises Inc", "D & H Enterprises, Inc.") should equal (0.95)
-        distance.call("My Gym Children's Fitness Center", "My Gym. Childrens Fitness") should equal (0.92)
-        distance.call("PENNSYLVANIA", "PENNCISYLVNIA") should equal (0.88)
+        distance.call("My Gym Children's Fitness Center", "My Gym. Childrens Fitness") should equal (0.942)
+        distance.call("PENNSYLVANIA", "PENNCISYLVNIA") should equal (0.898)
     }
 }
 
